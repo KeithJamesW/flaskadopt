074e4d76c2b07be722a264ea9997a7388b3d62e6
Aprochlometh <Aprochlometh@yahoo.com>
2022-06-16 12:04:43 -0600

    modified files

--- app.py
+++ app.py
@@ -6,32 +6,38 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
-
+print('test', )
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgres:///adopt")
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print('app', app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgres:///adopt")
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-connect_db(app)
-db.create_all()
+# connect_db(app)
+# db.create_all()
 
-# Having the Debug Toolbar show redirects explicitly is often useful;
-# however, if you want to turn it off, you can uncomment this line:
-#
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# # Having the Debug Toolbar show redirects explicitly is often useful;
+# # however, if you want to turn it off, you can uncomment this line:
+# #
+# # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 
 ##############################################################################
 
 
-@app.route("/")
-def list_pets():
-    """List all pets."""
+# @app.route("/")
+# def list_pets():
+#     """List all pets."""
 
-    pets = Pet.query.all()
-    return render_template("pet_list.html", pets=pets)
+#     pets = Pet.query.all()
+#     return render_template("pet_list.html", pets=pets)
+
+@app.route("/")
+def hello_world():
+    return "hello flask"
+
 
 
 @app.route("/add", methods=["GET", "POST"])
