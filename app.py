122c17692f0a8f7c3db3aad4a605d43912e6740c
Aprochlometh <Aprochlometh@yahoo.com>
2022-06-28 13:22:35 -0600

    modified files to exclude sqlalchemy

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
 print('test', )
 app = Flask(__name__)
-print('app', app)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgres:///adopt")
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# print('app', app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgres:///adopt")
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-connect_db(app)
-db.create_all()
+# connect_db(app)
+# db.create_all()
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -26,63 +26,66 @@ db.create_all()
 
 #############################################################################
 
-
 @app.route("/")
-def list_pets():
-    """List all pets."""
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-    pets = Pet.query.all()
-    return render_template("pet_list.html", pets=pets)
+# @app.route("/")
+# def list_pets():
+#     """List all pets."""
 
-
-
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_pet():
-    """Add a pet."""
-
-    form = AddPetForm()
-
-    if form.validate_on_submit():
-        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
-        new_pet = Pet(**data)
-        # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
-        db.session.add(new_pet)
-        db.session.commit()
-        flash(f"{new_pet.name} added.")
-        return redirect(url_for('list_pets'))
-
-    else:
-        # re-present form for editing
-        return render_template("pet_add_form.html", form=form)
+#     pets = Pet.query.all()
+#     return render_template("pet_list.html", pets=pets)
 
 
-@app.route("/<int:pet_id>", methods=["GET", "POST"])
-def edit_pet(pet_id):
-    """Edit pet."""
-
-    pet = Pet.query.get_or_404(pet_id)
-    form = EditPetForm(obj=pet)
-
-    if form.validate_on_submit():
-        pet.notes = form.notes.data
-        pet.available = form.available.data
-        pet.photo_url = form.photo_url.data
-        db.session.commit()
-        flash(f"{pet.name} updated.")
-        return redirect(url_for('list_pets'))
-
-    else:
-        # failed; re-present form for editing
-        return render_template("pet_edit_form.html", form=form, pet=pet)
 
 
-@app.route("/api/pets/<int:pet_id>", methods=['GET'])
-def api_get_pet(pet_id):
-    """Return basic info about pet in JSON."""
 
-    pet = Pet.query.get_or_404(pet_id)
-    info = {"name": pet.name, "age": pet.age}
+# @app.route("/add", methods=["GET", "POST"])
+# def add_pet():
+#     """Add a pet."""
 
-    return jsonify(info)
+#     form = AddPetForm()
+
+#     if form.validate_on_submit():
+#         data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+#         new_pet = Pet(**data)
+#         # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+#         db.session.add(new_pet)
+#         db.session.commit()
+#         flash(f"{new_pet.name} added.")
+#         return redirect(url_for('list_pets'))
+
+#     else:
+#         # re-present form for editing
+#         return render_template("pet_add_form.html", form=form)
+
+
+# @app.route("/<int:pet_id>", methods=["GET", "POST"])
+# def edit_pet(pet_id):
+#     """Edit pet."""
+
+#     pet = Pet.query.get_or_404(pet_id)
+#     form = EditPetForm(obj=pet)
+
+#     if form.validate_on_submit():
+#         pet.notes = form.notes.data
+#         pet.available = form.available.data
+#         pet.photo_url = form.photo_url.data
+#         db.session.commit()
+#         flash(f"{pet.name} updated.")
+#         return redirect(url_for('list_pets'))
+
+#     else:
+#         # failed; re-present form for editing
+#         return render_template("pet_edit_form.html", form=form, pet=pet)
+
+
+# @app.route("/api/pets/<int:pet_id>", methods=['GET'])
+# def api_get_pet(pet_id):
+#     """Return basic info about pet in JSON."""
+
+#     pet = Pet.query.get_or_404(pet_id)
+#     info = {"name": pet.name, "age": pet.age}
+
+#     return jsonify(info)
