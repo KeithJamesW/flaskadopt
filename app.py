fe3596708495333ca945fcccbe5c77c551901199
Aprochlometh <Aprochlometh@yahoo.com>
2022-06-28 13:37:26 -0600

    commented out runtime file

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+if __name__ == '__main__':
+    app.run()
+
 # @app.route("/")
 # def list_pets():
 #     """List all pets."""
